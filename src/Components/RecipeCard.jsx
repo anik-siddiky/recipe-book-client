0d894c04158a6ac2bc9c9b89f67a19d7dc6a6415
Anik Siddiky <codesbynik@gmail.com>
2025-05-21 22:32:27 +0600

    did some edit on recipe card

--- src/Components/RecipeCard.jsx
+++ src/Components/RecipeCard.jsx
@@ -10,16 +10,18 @@ const RecipeCard = ({ recipe }) => {
 
 
     return (
-        <div className="bg-white shadow-md overflow-hidden hover:shadow-lg transition duration-300">
-            <img
-                src={image || placeholderImage}
-                alt={title}
-                className="w-full h-48 object-cover"
-            />
+        <div className="bg-gray-100 shadow-xl overflow-hidden hover:shadow-lg transition duration-300 rounded-sm">
+            <div>
+                <img
+                    src={image || placeholderImage}
+                    alt={title}
+                    className="w-full h-56 object-cover"
+                />
+            </div>
             <div className="p-4 space-y-2">
                 <h3 className="text-xl font-semibold text-gray-800">{title}</h3>
-                <p className="text-sm text-gray-500">Cuisine: {cuisineType}</p>
-                <p className="text-sm text-gray-500 flex items-center gap-1"><AiFillLike size={18} color='#ED1C24' /> Likes: {likeCount}</p>
+                <p className="text-sm text-gray-700">Cuisine: {cuisineType}</p>
+                <p className="text-sm text-gray-700 flex items-center gap-1"><AiFillLike size={18} color='#ED1C24' /> Likes: {likeCount}</p>
                 <Link to={`/recipe-details/${_id}`}>
                     <button className="w-full mt-3 bg-[#ED1C24] text-white px-4 py-2 rounded-md hover:bg-red-700 transition"
                     >View Details</button>
